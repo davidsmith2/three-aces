7ac192e2ac792567bce15c63cf9e80585364b161
davidsmith2 <davidsmith2@gmail.com>
2014-03-02 19:47:43 -0500

    initial revision

---
+++ server/app.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var async = require('async');
+var hbs = require('express-hbs');
+
+var mongoose = require('mongoose');
+
+
+// start mongoose
+mongoose.connect('mongodb://localhost/three-aces');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+
+	/* schemas */
+    var menuItemSizeSchema = new mongoose.Schema({
+        name: String,
+        price: Number
+    });
+
+    var menuItemSchema = new mongoose.Schema({
+        name: String,
+        description: String,
+        category: String,
+        sizes: [menuItemSizeSchema]
+    });
+
+    /* models */
+    var MenuItemSize = mongoose.model( 'menuItemSize', menuItemSizeSchema );
+    var MenuItem = mongoose.model( 'menuItem', menuItemSchema );
+
+	var app = express();
+
+	app.configure(function(){
+	    app.set('port', 9000);
+	    app.set('view engine', 'handlebars');
+	    app.set('views', __dirname + '../app/scripts/views');
+        app.use(express.bodyParser());
+	});
+
+	// simple log
+	app.use(function(req, res, next){
+        console.log('%s %s', req.method, req.url);
+        next();
+	});
+
+	// mount static
+	app.use(express.static( path.join( __dirname, '../app') ));
+	app.use(express.static( path.join( __dirname, '../.tmp') ));
+
+
+	// route index.html
+	app.get('/', function(req, res){
+        res.sendfile( path.join( __dirname, '../app/index.html' ) );
+	});
+
+    // api
+    app.get('/api', function(req, res){
+        res.send( 'api is running' );
+    });
+
+    app.get('/api/menu-item', function(req, res){
+        return MenuItem.find(function(err, menuItems){
+            if (!err) {
+                return res.send(menuItems);
+            } else {
+                return console.log(err);
+            }
+        });
+    });
+
+    app.post('/api/menu-item', function(req, res){
+        var menuItem = new MenuItem({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            sizes: req.body.sizes
+        });
+        menuItem.save(function(err){
+            if (!err) {
+                return console.log('post');
+            } else {
+                return console.log(err);
+            }
+        });
+        return res.send(menuItem);
+    });
+
+    app.put('/api/menu-item/:id', function(req, res){
+        return MenuItem.findById(req.params.id, function(err, menuItem){
+            menuItem.name = req.body.name;
+            menuItem.description = req.body.description;
+            menuItem.category = req.body.category;
+            menuItem.sizes = req.body.sizes;
+            return menuItem.save(function(err){
+                if (!err) {
+                    console.log('put');
+                } else {
+                    console.log(err);
+                }
+                return res.send(menuItem);
+            });
+        });
+    });
+
+    app['delete']('/api/menu-item/:id', function(req, res){
+        return MenuItem.findById(req.params.id, function(err, menuItem){
+            return menuItem.remove(function(err){
+                if (!err) {
+                    console.log('delete');
+                    return res.send('');
+                } else {
+                    console.log(err);
+                }
+            });
+        });
+    });
+
+    app.get('/api/menu-item-size', function(req, res){
+        return MenuItemSize.find(function(err, menuItemSizes){
+            if (!err) {
+                return res.send(menuItemSizes);
+            } else {
+                return console.log(err);
+            }
+        });
+    });
+
+    app.post('/api/menu-item-size', function(req, res){
+        var menuItemSize = new MenuItemSize({
+            name: req.body.name,
+            price: req.body.price
+        });
+        menuItemSize.save(function(err){
+            if (!err) {
+                return console.log('created');
+            } else {
+                return console.log(err);
+            }
+        });
+        return res.send(menuItemSize);
+    });
+
+	// start server
+	http.createServer(app).listen(app.get('port'), function(){
+	    console.log('Express App started!');
+	});
+});
+
+

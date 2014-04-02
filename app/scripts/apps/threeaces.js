85afa714b5b5becfe8923f53273cdf3447301af0
davidsmith2 <davidsmith2@gmail.com>
2014-04-02 00:45:14 -0400

    tidy up

--- app/scripts/apps/threeaces.js
+++ app/scripts/apps/threeaces.js
@@ -3,43 +3,36 @@ define([
     'backbone.marionette',
     'jquery',
     'underscore',
-    'apps/threeaces.vent',
-    'apps/threeaces.layout',
+    'apps/private/threeaces.privateapp',
     'apps/threeaces.datamanager',
-    'apps/private/threeaces.privateapp'
-], function (Backbone, Marionette, $, _, appVent, Layout, DataManager, privateApp) {
+    'apps/threeaces.layout',
+    'apps/threeaces.vent'
+], function (Backbone, Marionette, $, _, privateApp, DataManager, Layout, appVent) {
     'use strict';
-
     var ThreeAces = new Marionette.Application();
-
     ThreeAces.addRegions({
         content: '#content'
     });
-
     appVent.on('layout:rendered', function () {
         console.log('layout:rendered');
         Backbone.history.start();
     });
-
+/*
     appVent.on('publicApp:show', function (layout) {
         console.log('publicApp:show');
-        //publicApp.layout(layout);
+        publicApp.layout(layout);
     });
-
+*/
     appVent.on('privateApp:show', function (layout) {
         console.log('privateApp:show');
         privateApp.init(layout);
     });
-
-    // data initializer
     ThreeAces.addInitializer(function () {
         $.when(DataManager.getRestaurants()).done(function (restaurants) {
             privateApp.setData(restaurants);
             //publicApp.data(data);
         });
     });
-
-    // layout initializer
     ThreeAces.addInitializer(function () {
         ThreeAces.layout = new Layout();
         ThreeAces.layout.on('show', function () {
@@ -47,7 +40,5 @@ define([
         });
         ThreeAces.content.show(ThreeAces.layout);
     });
-
     return ThreeAces;
-
 });

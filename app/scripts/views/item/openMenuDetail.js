69dd4e4cccb1bb9d3269f90f28abb3db6141dcb9
davidsmith2 <davidsmith2@gmail.com>
2014-04-03 10:39:12 -0400

    open menu detail view

--- app/scripts/views/item/openMenuDetail.js
+++ app/scripts/views/item/openMenuDetail.js
@@ -5,10 +5,8 @@ define([
     'underscore',
     'apps/private/threeaces.privateapp.vent',
     'hbs!tmpl/item/openMenuDetail',
-    'views/item/restaurantInfo',
-    'views/item/environmentInfo',
     'views/item/addRestaurant'
-], function (Backbone, Marionette, $, _, privateAppVent, OpenMenuDetailTmpl, RestaurantInfoView, EnvironmentInfoView, AddRestaurantView) {
+], function (Backbone, Marionette, $, _, privateAppVent, OpenMenuDetailTmpl, AddRestaurantView) {
     'use strict';
 	return Backbone.Marionette.ItemView.extend({
         template: OpenMenuDetailTmpl,
@@ -16,17 +14,9 @@ define([
         events: {
         },
         initialize: function () {
-            this.restaurantInfoView = new RestaurantInfoView({
-                model: this.model.get('restaurantInfo')
-            });
-            this.environmentInfoView = new EnvironmentInfoView({
-                model: this.model.get('environmentInfo')
-            });
             privateAppVent.on('restaurantInfo:edit', this.onRestaurantInfoEdit, this);
         },
         onRender: function () {
-            this.$('#restaurantInfo').append(this.restaurantInfoView.render().el);
-            this.$('#environmentInfo').append(this.environmentInfoView.render().el);
         },
         onRestaurantInfoEdit: function (options) {
             this.addRestaurantView = new AddRestaurantView({

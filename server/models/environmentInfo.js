d96aa77c0978d7553290e89d2928127316de5f69
davidsmith2 <davidsmith2@gmail.com>
2014-04-09 01:10:47 -0400

    deleting old files

--- server/models/environmentInfo.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var environmentInfoSchema = new mongoose.Schema({
-    takeoutAvailable: Boolean
-});
-
-module.exports = mongoose.model('environmentInfo', environmentInfoSchema);

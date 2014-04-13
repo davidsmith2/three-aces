ffdd3ab84307d8007c1910af8673137f66087c28
davidsmith2 <davidsmith2@gmail.com>
2014-04-12 20:41:49 -0400

    deleting old files

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-    'use strict';
-    require('./openMenus')(app);
-    //require('./menus')(app);
-    //require('./menuGroups')(app);
-    //require('./menuItems')(app);
-    //require('./menuItemSizes')(app);
-};

af92dfe519806488ca1587cde4ebc5c090c56021
davidsmith2 <davidsmith2@gmail.com>
2014-04-14 00:26:49 -0400

    making this a little more readable

--- server/controllers/menus/index.js
+++ server/controllers/menus/index.js
@@ -4,12 +4,15 @@ var controller = require('../generic/index.js')(Model);
 
 module.exports = _.extend(controller, {
     index: function (req, res) {
-        return Model.where('openMenu').equals(req.params.openmenu).find(function (err, models) {
-            if (!err) {
-                return res.send(models);
-            } else {
-                return console.log(err);
-            }
-        });
+        return Model
+            .where('openMenu')
+            .equals(req.params.openmenu)
+            .find(function (err, models) {
+                if (!err) {
+                    return res.send(models);
+                } else {
+                    return console.log(err);
+                }
+            });
     }
 });

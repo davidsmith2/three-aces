575f2cf1cbef8f793736f24ffa5e0c93347da8f2
davidsmith2 <davidsmith2@gmail.com>
2014-04-14 21:53:17 -0400

    moving private screens into private folder

--- app/scripts/screens/environment/views/form.js
+++
@@ -1,7 +0,0 @@
-define([
-    'views/generic/form'
-], function (FormView) {
-    'use strict';
-	var AddEnvironmentView = FormView.extend();
-    return AddEnvironmentView;
-});
--- app/scripts/screens/restaurant/views/form.js
+++
@@ -1,7 +0,0 @@
-define([
-    'views/generic/form'
-], function (FormView) {
-    'use strict';
-	var AddRestaurantView = FormView.extend();
-    return AddRestaurantView;
-});

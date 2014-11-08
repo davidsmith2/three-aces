6c5fe35b21efa4c54a2f0195e9e7da03878f7330
unknown <dsmith@PF00YDC2.Org.Collegeboard.local>
2014-11-08 14:45:39 -0500

    refine command pattern for displaying panels

--- app/scripts/apps/private/apps/open_menus/actions/index.js
+++ app/scripts/apps/private/apps/open_menus/actions/index.js
@@ -2,30 +2,29 @@ define([
 	'jquery',
 	'app',
 	'apps/private/apps/open_menus/index/views/open_menus_heading',
-	'apps/private/apps/open_menus/index/views/open_menus_table',
-	'apps/private/common/views/panel/panel'
+	'apps/private/apps/open_menus/index/views/open_menus_table'
 ], function ($, App, OpenMenusHeadingView, OpenMenusTableView) {
 	return function () {
 		$.when(App.request('openMenu:entities')).done(function (openMenus) {
-			App.execute('panel:show', {
+			var openMenusHeadingView = new OpenMenusHeadingView(),
+				openMenusTableView = new OpenMenusTableView({collection: openMenus});
+			openMenusHeadingView.on('openMenu:new', function () {
+				App.vent.trigger('openMenu:new');
+			});
+			openMenusTableView.on('childview:openMenu:show', function (itemView, options) {
+				App.vent.trigger('openMenu:show', options.model.get('_id'));
+			});
+			openMenusTableView.on('childview:openMenu:delete', function (itemView, options) {
+				App.vent.trigger('openMenu:delete', options.model.get('_id'));
+			});
+			App.execute('panel:show:1', {
 				region: App.mainRegion,
-				callback: function (panel) {
-					var openMenusHeadingView = new OpenMenusHeadingView();
-					var openMenusTableView = new OpenMenusTableView({
-						collection: openMenus
-					});
-					openMenusHeadingView.on('openMenu:new', function () {
-						App.vent.trigger('openMenu:new');
-					});
-					openMenusTableView.on('childview:openMenu:show', function (itemView, options) {
-						App.vent.trigger('openMenu:show', options.model.get('_id'));
-					});
-					openMenusTableView.on('childview:openMenu:delete', function (itemView, options) {
-						App.vent.trigger('openMenu:delete', options.model.get('_id'));
-					});
-					panel.ui.heading.addClass('clearfix');
-					panel.headingRegion.show(openMenusHeadingView);
-					panel.bodyRegion.show(openMenusTableView);
+				headingView: openMenusHeadingView,
+				bodyView: openMenusTableView,
+				callbacks: {
+					onRender: function (panelView) {
+						panelView.ui.heading.addClass('clearfix');
+					}
 				}
 			});
 		});
--- app/scripts/apps/private/common/views/panel/panel.js
+++ app/scripts/apps/private/common/views/panel/panel.js
@@ -21,13 +21,28 @@ define([
 			if (this.options.callback) {
 				this.options.callback(this);
 			}
+			if (this.options.callbacks && this.options.callbacks.onRender) {
+				this.options.callbacks.onRender(this);
+			}
 		}
 	});
+
+	var createPanel = function (options) {
+		return new PanelView(options);
+	};
+
 	var API = {
-		showPanel: function (options) {
-			var panelView = new PanelView(options);
+		show: function (options) {
+			var panelView = createPanel(options);
 			options.region.show(panelView);
+		},
+		show_1: function (options) {
+			var panelView = createPanel(options);
+			options.region.show(panelView);
+			panelView.headingRegion.show(options.headingView);
+			panelView.bodyRegion.show(options.bodyView);
 		}
 	};
-	App.commands.setHandler('panel:show', API.showPanel);
+	App.commands.setHandler('panel:show', API.show);
+	App.commands.setHandler('panel:show:1', API.show_1);
 });
